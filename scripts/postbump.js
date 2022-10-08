d7ffdbf54d0587f82c0a7a46c5e7499fd2cd07d1
Bernhard Pottler <bpottler@nexgo.de>
2022-10-08 17:00:36 +0200

    build: add updated readme.md to commit on 'release'

--- scripts/postbump.js
+++ scripts/postbump.js
@@ -1,18 +1,23 @@
 // precommit.js
 // update the version of license-report-recurse in the README.md file
+// when running "npm run release"
 
+import cp from 'node:child_process';
 import fs from 'fs';
 import path from 'node:path';
 import url from 'node:url';
+import util from 'node:util';
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const execAsPromise = util.promisify(cp.exec)
 
 const packageJsonPath = path
 	.resolve(__dirname, '..', 'package.json')
 	.replace(/(\s+)/g, '\\$1');
 
-const readmePath = path
-	.resolve(__dirname, '..', 'README.md')
+  const readmeFileName = 'README.md'
+  const readmePath = path
+	.resolve(__dirname, '..', readmeFileName)
 	.replace(/(\s+)/g, '\\$1');
 
 (async () => {
@@ -32,6 +37,10 @@ const readmePath = path
   if (regexResult?.length === 4) {
     newReadmeContent = [regexResult[1], licenseReportRecurseVersion, regexResult[3]].join('');
     await fs.promises.writeFile(readmePath, newReadmeContent);
+    const { stderr } = await execAsPromise(`git add ${readmeFileName}`)
+    if (stderr) {
+      console.error(`Error when adding ${readmeFileName} to git repository.`)
+    }
   } else {
     console.error('Error: pattern in README.md not found.')
   }
